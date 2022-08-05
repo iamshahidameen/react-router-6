1b00408cc6438670924a5b7775651e2e1f4923b6
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-06 03:17:18 +0500

    styled the navbar link with NavLink style object isActive

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const StyledNavbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        style={({ isActive }) => {
+          return {
+            color: isActive ? 'yellow' : 'green',
+            fontSize: isActive ? '16px' : '24px',
+          };
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
+    </nav>
+  );
+};
+
+export default StyledNavbar;
