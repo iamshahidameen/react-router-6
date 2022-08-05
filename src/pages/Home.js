86f1048fb8b9f2af76e4743673d256fc36aab641
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-06 03:07:28 +0500

    created navbar using sharedLayouts, Outlets and index props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,7 @@
-import { Link, link } from 'react-router-dom';
 const Home = () => {
   return (
     <section className="section">
-      <h2>Home Page</h2>
-      <Link to="/about" className="btn">
-        About
-      </Link>
+      <h2>Hello Jee</h2>
     </section>
   );
 };
