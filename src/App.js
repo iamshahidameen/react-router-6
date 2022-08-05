c42f589f9b6b8f3108b91299467c848626478705
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-05 22:49:49 +0500

    Setup router basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h2>React Router 6 Tutorial</h2>} />
+        <Route path="/about" element={<h2>About</h2>} />
+        <Route path="/test" element={<h2>This is test</h2>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
