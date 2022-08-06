c4f134ec0b7bb28debc8b17a66dc03924d44a692
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-06 19:15:06 +0500

    print the productID (useParams) on every single page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,10 @@
+import { useParams } from 'react-router-dom';
 const SingleProduct = () => {
+  console.log(useParams());
+  const { productID } = useParams();
   return (
-    <section className='section product'>
-      <h2>single product</h2>
+    <section className="section product">
+      <h2>{productID}</h2>
     </section>
   );
 };
