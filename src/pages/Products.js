332d2a466d330deabaa702bcb897f8d084389e48
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-06 19:43:44 +0500

    setup Single Product  page using useParam id and matching it with the data.js product id using find() method

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,25 @@
+import { Link } from 'react-router-dom';
+import productsList from '../data';
+import SingleProduct from './SingleProduct';
 const Products = () => {
   return (
     <>
-      <section className='section'>
+      <section className="section">
         <h2>products</h2>
+        <div className="products">
+          {productsList.map((product) => {
+            const { id, name, image } = product;
+            return (
+              <article key={id}>
+                <h5>{name}</h5>
+                <Link to={`/products/${id}`} element>
+                  {' '}
+                  More Info{' '}
+                </Link>
+              </article>
+            );
+          })}
+        </div>
       </section>
     </>
   );
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,10 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
 const SingleProduct = () => {
-  console.log(useParams());
   const { productID } = useParams();
+  const product = products.find((prod) => prod.id === productID);
+  const { id, name, image } = product;
   return (
-    <section className="section product">
-      <h2>{productID}</h2>
+    <section className="section product" id={id}>
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+
+      <Link to="/products">Back to Products</Link>
     </section>
   );
 };
